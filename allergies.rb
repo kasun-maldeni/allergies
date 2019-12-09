ae07cb1cc8ecdf01b791c739094521058efd41c0
Kasun <kasun.maldeni@gmail.com>
2019-12-09 11:54:57 +1100

    add tests and implementation for Allergies to return correct results for a score of 0

--- allergies.rb
+++ allergies.rb
@@ -1,2 +1,13 @@
+# frozen_string_literal: true
+
 class Allergies
+  def initialize(score); end
+
+  def allergic_to?(_allergen)
+    false
+  end
+
+  def list
+    []
+  end
 end
--- spec/allergies_spec.rb
+++ spec/allergies_spec.rb
@@ -1,3 +1,15 @@
+# frozen_string_literal: true
+
 require_relative '../allergies.rb'
 
-describe Allergies
+describe Allergies do
+  it 'should return false for chocolate when score is equal to 0' do
+    allergies = Allergies.new(0)
+    expect(allergies.allergic_to?('chocolate')).to be false
+  end
+
+  it 'should return an empty list when score is equal to 0' do
+    allergies = Allergies.new(0)
+    expect(allergies.list).to eq []
+  end
+end
