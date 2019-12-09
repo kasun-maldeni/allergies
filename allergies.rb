e639bc38bd95e9c0dfedfddf488fb7b34953a3e3
Kasun <kasun.maldeni@gmail.com>
2019-12-10 00:21:53 +1100

    Implement Allergies to pass all unit tests

--- allergies.rb
+++ allergies.rb
@@ -1,13 +1,24 @@
-# frozen_string_literal: true
-
 class Allergies
-  def initialize(score); end
+  NAMES = %w[eggs peanuts shellfish strawberries tomatoes chocolate pollen cats]
 
-  def allergic_to?(_allergen)
-    false
+  def initialize(score)
+    raise 'invalid score' unless
+      score.is_a?(Integer) &&
+      score >= 0 &&
+      score < 256
+
+    @score = score
+  end
+
+  def allergic_to?(allergen)
+    list.include? allergen
   end
 
   def list
-    []
+    score = @score
+    NAMES.reverse.select.with_index do |allergen, i|
+      allergen_score = 2**(NAMES.length - 1 - i)
+      score -= allergen_score if score >= allergen_score
+    end
   end
 end
